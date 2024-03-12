27914fb4971f8ac663cdcca601685e3d944e65ed
Speirsy <jpspeirs@icloud.com>
2024-03-12 14:50:07 +0000

    some working css

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,18 +1,20 @@
+import React from 'react';
+import './Home.css';
+
 const Home = () => (
   
-  <div>
-    <h1>FRAMING NORAH</h1>
-        <h4>Picture Hanger</h4>
-        <p>An expert, clean and safe service to get your art and frames up on your walls</p>
-        <button onClick={() => window.location.href = '/contact'}>Tell Us</button>
-        <h4>Frame Therapist</h4>
-        <p>DIY framing and ‘off the shelf’ frames can be frustrating, but results can be improved with a little love from an expert. I will re-mount and re-assemble your frames, on site, to display your beloved pictures at their very best, for you to enjoy.   </p>
-        <button onClick={() => window.location.href = '/contact'}>What's Gone Wrong?</button>
 
-  </div>
-      
-    );
+    <div className="centered">
+      <h1>FRAMING NORAH</h1>
+      <h4>Picture Hanger</h4>
+      <p>An expert, clean and safe service to get your art and frames up on your walls</p>
+      <button onClick={() => window.location.href = '/contact'}>Tell Us</button>
+      <h4>Frame Therapist</h4>
+      <p>DIY framing and ‘off the shelf’ frames can be frustrating, but results can be improved with a little love from an expert. I will re-mount and re-assemble your frames, on site, to display your beloved pictures at their very best, for you to enjoy.   </p>
+      <button onClick={() => window.location.href = '/contact'}>What's Gone Wrong?</button>
+    </div>
+  );
 
-    export default Home;
+  export default Home;
     
 
