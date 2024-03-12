875c0cde05cd296d1f2b717024e6a6af0a0c5f79
Speirsy <jpspeirs@icloud.com>
2024-03-12 18:21:18 +0000

    NavBar css and Burger toggle working

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,26 +1,41 @@
+// NavBar.js
+
+import React, { useState } from "react";
 import { Link, useNavigate } from "react-router-dom";
+import './NavBar.css';
 
 const NavBar = () => {
   const navigate = useNavigate();
   const goBack = () => navigate(-1);
 
+  const [showMenu, setShowMenu] = useState(false);
+
   return (
-    <ul>
-      <li>
-        <Link to="/">Home</Link>
-      </li>
-      <li>
-        <Link to="/about">About</Link>
-      </li>
-      <li>
-        <Link to="/contact">Contact</Link>
-      </li>
-    
-      <li>
-        <button onClick={goBack}>Back</button>
-      </li>
-    </ul>
+    <div className="navbar">
+      <ul className={showMenu ? "nav-menu active" : "nav-menu"}>
+        <li>
+          <Link to="/">Home</Link>
+        </li>
+        <li>
+          <Link to="/about">About</Link>
+        </li>
+        <li>
+          <Link to="/contact">Contact</Link>
+        </li>
+      
+        <li>
+          <button onClick={goBack}>Back</button>
+        </li>
+      </ul>
+
+      <div className="burger" onClick={() => setShowMenu(!showMenu)}>
+        <div className={showMenu ? "line line1 open" : "line"}></div>
+        <div className={showMenu ? "line line2 open" : "line"}></div>
+        <div className={showMenu ? "line line3 open" : "line"}></div>
+      </div>
+    </div>
   );
 };
 
 export default NavBar;
+  
