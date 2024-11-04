27bd08d47145b88bea9625afee3e10049c61afc3
Speirsy <jpspeirs@icloud.com>
2024-11-04 10:27:42 +0000

    contact changes

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -9,6 +9,8 @@ const Contact = () => {
         <h2>Contact Us</h2>
         <p>For any inquiries, please fill out the form below.</p>
         <ContactForm />
+        <h3>Alternatively you can call us on 07989226593</h3>
+        <h3>or email us at john@framingnorah.com</h3>
       </div>
     </>
   );
